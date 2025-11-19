43516fb904ae44ebf4f9a21d28d7b3c8e120d3e4
hieudm05 <hieudm05@gmail.com>
2025-11-19 11:19:57 +0700

    sử dụng ref khai báo state

---
+++ 03-reactivity/src/App.vue
@@ -0,0 +1,11 @@
+
+<script setup>
+import { ref } from 'vue';
+const count = ref(0)
+const increase = () => count.value++
+</script>
+<template>
+  <!-- Reactivity (Tính phản ứng) -->
+  <div>{{ count }}</div>
+  <button @click="increase">Increase</button>
+</template>
