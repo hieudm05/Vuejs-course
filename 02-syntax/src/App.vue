3e5d3b9326071a35717210ef65b22ee91113c460
hieudm05 <hieudm05@gmail.com>
2025-11-19 10:34:19 +0700

    syntax, v-html, bind

---
+++ 02-syntax/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+const message = "hello world";
+const raw = `<span style="color: red;">v-html</span>`;
+const count = 1;
+const isbuttonDisable = true
+const objectAtribute = {
+  id: 'container',
+  class: "child",
+  style: {
+    color: 'blue'
+  }
+}
+</script>
+
+<template>
+  <h1>Syntax</h1>
+  <p>{{ message }}</p>
+  <p v-html="raw"></p>
+  <p>{{ count }}</p>
+  <!-- bind -->
+  <button :id="count" :disabled="isbuttonDisable">Bấm</button>
+  <!-- Ràng buộc nhiều thuộc tính động -->
+  <span v-bind="objectAtribute">Ràng buộc động</span>
+</template>
