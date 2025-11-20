d0fba1baf28bc7a74cee0c5fd76ab761628a243a
hieudm05 <hieudm05@gmail.com>
2025-11-20 12:36:27 +0700

    style binding

---
+++ 05-class_style-binding/src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { computed, ref } from "vue";
+
+const isActive = ref(true);
+const hasError = ref(false);
+
+const isStatus = computed(() => ({
+  active: isActive.value && !hasError.value,
+  "has-error": hasError.value,
+}));
+const changeActive = () => {
+  isActive.value = !isActive.value;
+};
+</script>
+<template>
+  <!-- Lý do 'has-error' phải để trong dấu nháy vì nó có ký tự đặc biệt là "-" -->
+  <button @click="changeActive" class="btn" :class="isStatus">Nút</button>
+</template>
+<style>
+.btn {
+  background-color: peru;
+  border-radius: 10px;
+  padding: 10px 20px;
+  border: none;
+}
+.active {
+  background-color: blue;
+  color: white;
+}
+.has-error {
+  background-color: red;
+  color: white;
+}
+</style>
