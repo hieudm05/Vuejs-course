888ccd02c3877f91ccb5e359853d9ffe5b85027a
hieudm05 <hieudm05@gmail.com>
2025-11-20 11:33:43 +0700

    writable: ghi đè computed

--- 04-computed/src/App.vue
+++ 04-computed/src/App.vue
@@ -1,21 +1,24 @@
 <script setup>
-import { computed, reactive } from 'vue';
+import { computed, reactive, ref } from "vue";
 
-  const books = reactive({
-    name: 'book1',
-    chapters: []
-  })
-  // computed:
-  // Tính hiểu quả
-  // + Nó sẽ tính toán logic 1 phần data trong 1 object cần xử lí
-  // + Nó chỉ tính toán lại nếu các phụ thuộc nó thay đổi thôi 
-  // Tính phản ứng (reactivity): giúp cập nhật state và giao diện mượt mà và kịp thời
-  // Tính tài sử dụng code
-
-  const idPublic = computed(() => books.chapters.length ? "Có":"Không")
-  
+const fisrtName = ref("Nguyễn Văn");
+const lastName = ref("A");
+const fullName = computed({
+  get() {
+    return fisrtName.value + " " + lastName.value;
+  },
+  set(newVal) {
+    [fisrtName.value, lastName.value] = newVal.split(",")
+  },
+});
+// writable: ghi đè trong computed
+// Vì trong computed chỉ xử lí đọc dữ liệu, nên là nếu để thêm data hoặc thay đổi data thì phải xử dụng getter and setter
+const changeFullname = () => {
+  return fullName.value = "Đoàn Di,Băng"
+}
 </script>
 <template>
-  <p>Tên sách: {{ books.name }}</p>
-  <p>Xuất bản: {{ idPublic }}</p>
+  <h1>Writable</h1>
+  <p>Full Name {{ fullName }}</p>
+  <button @click="changeFullname">Thay đổi</button>
 </template>
