25b51bbffb56e1358d8f9c33c0f0689962821d67
hieudm05 <hieudm05@gmail.com>
2025-11-20 11:10:06 +0700

    computed

---
+++ 04-computed/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { computed, reactive } from 'vue';
+
+  const books = reactive({
+    name: 'book1',
+    chapters: []
+  })
+  // computed:
+  // Tính hiểu quả
+  // + Nó sẽ tính toán logic 1 phần data trong 1 object cần xử lí
+  // + Nó chỉ tính toán lại nếu các phụ thuộc nó thay đổi thôi 
+  // Tính phản ứng (reactivity): giúp cập nhật state và giao diện mượt mà và kịp thời
+  // Tính tài sử dụng code
+
+  const idPublic = computed(() => books.chapters.length ? "Có":"Không")
+  
+</script>
+<template>
+  <p>Tên sách: {{ books.name }}</p>
+  <p>Xuất bản: {{ idPublic }}</p>
+</template>
