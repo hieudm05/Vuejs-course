8cb7768a248730ef51bd816b492441623ef317f6
hieudm05 <hieudm05@gmail.com>
2025-11-20 10:32:47 +0700

    deep reactive

--- 03-reactivity/src/App.vue
+++ 03-reactivity/src/App.vue
@@ -1,11 +1,34 @@
-
 <script setup>
-import { ref } from 'vue';
-const count = ref(0)
-const increase = () => count.value++
+import { reactive, ref } from "vue";
+// const count = ref(0);
+
+// const object = ref({
+//   count: 1,
+//   user: {
+//     age: 18,
+//   },
+//   array: [1, 2, 3],
+// });
+// const change = () => {
+//   object.value.count++
+// };
+
+// Nếu với mảng hay object nên dùng reactive thì không cần .value nữa
+
+const object = reactive({
+  count: 1,
+  user: {
+    age: 18,
+  },
+  array: [1, 2, 3],
+});
+const change = () => {
+  object.count++
+};
+// const increase = () => count.value++;
 </script>
 <template>
   <!-- Reactivity (Tính phản ứng) -->
-  <div>{{ count }}</div>
-  <button @click="increase">Increase</button>
+  <div>{{ object.count }}</div>
+  <button @click="change">Increase</button>
 </template>
