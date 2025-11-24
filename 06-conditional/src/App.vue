fd896d838d12b4c3f208c6beff08228d848cb0cc
hieudm05 <hieudm05@gmail.com>
2025-11-24 10:05:24 +0700

    v-if, v-show

---
+++ 06-conditional/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+
+const isLoginedIn = ref(false)
+const showHide = ref(false);
+const changeStatus = () => {
+  isLoginedIn.value = !isLoginedIn.value;
+}
+const showHidden = () => {
+  showHide.value = !showHide.value;
+}
+</script>
+<template>
+  <p v-if="isLoginedIn">Chào mừng bạn đến trang hệ thống.</p>
+  <p v-else>Vui lòng đăng nhập để tiếp tục.</p>
+  <button @click="changeStatus">Change status</button>
+  <hr />
+  <h2>Test show hide</h2>
+  <!-- v-show: nhằm mục đích ẩn hiện phần tử bằng cách thêm style thuộc tính  display: none (nếu ẩn) -->
+  <p v-show="showHide">{{ showHide ? "Đã hiện" : "Đã ẩn" }}</p>
+  <button @click="showHidden">Show hiden</button>
+</template>
+
