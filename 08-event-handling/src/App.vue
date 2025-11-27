cc7e1d6974844975c106c7db79faa06a610cc035
hieudm05 <hieudm05@gmail.com>
2025-11-27 09:56:46 +0700

    xử lý sự kiện trong vue

---
+++ 08-event-handling/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+const greet = (event) => {
+  console.log(event.target);
+  alert(`Hello! Event type: ${event.type}`);
+}
+const sayHi = (name) => {
+  alert(`Hi ${name}`);
+}
+
+</script>
+<template>
+  <h2>Event handling</h2>
+  <p>Count: {{ count }}</p>
+  <button @click="count++">Tăng</button>
+  <button @click="greet">event</button>
+  <button @click="sayHi('Vue Learner')">Say Hi</button>
+</template>
