4b6bc75210a86cfbb7b2006c74017d466b55ee57
hieudm05 <hieudm05@gmail.com>
2025-11-27 14:07:17 +0700

    v-model

---
+++ 09-form-input-bindings/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue';
+
+const text = ref('Xin chào');
+
+</script>
+<template>
+  <h2>Form Input Bindings</h2>
+  <p>{{ text }}</p>
+   <input v-model="text" />
+</template>
