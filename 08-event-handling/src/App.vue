5265f8a643ca7d772de2ce589fea1bc84197fa72
hieudm05 <hieudm05@gmail.com>
2025-11-27 10:23:40 +0700

    key modifier

--- 08-event-handling/src/App.vue
+++ 08-event-handling/src/App.vue
@@ -1,27 +1,10 @@
 <script setup>
-const sayHi1 = () => {
-  alert(`Hi 1`);
-};
-const sayHi2 = () => {
-  alert(`Hi 2`);
-};
-const handleSubmit = (event) => {
-  alert('Form submission prevented!');
-};
+const message = () => {
+  alert('Form submitted!');
+}
 </script>
 <template>
-  <h2>Event handling</h2>
-    <!-- Sử dụng đối số event "stop" để ngăn chặn event của phần con không bị lan ra phần cha -->
-  <!-- <button @click="sayHi1">
-    <button @click.stop="sayHi2">Say Hi 2</button>
-    Say Hi 1
-  </button> -->
-  <!-- <form @submit.prevent.stop="handleSubmit">
-    <button type="submit">Submit</button>
-  </form> -->
-  <!-- "self" nó kiểm tra xem mình ấn vào sự kiện nào -->
-  <button @click="sayHi1">
-    <button @click.self="sayHi2">Say Hi 2</button>
-    Say Hi 1
-  </button>
+  <h2>Key modifier</h2>
+  <input @keyup.enter="message" type="text" placeholder="Press Enter to submit" />
+  <button type="submit">Submit</button>
 </template>
