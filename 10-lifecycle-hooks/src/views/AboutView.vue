22510734d60d425d762740c474891ece288911a3
hieudm05 <hieudm05@gmail.com>
2025-12-03 10:32:53 +0700

    onBeforeMount, onBeforeUnmount

--- 10-lifecycle-hooks/src/views/AboutView.vue
+++ 10-lifecycle-hooks/src/views/AboutView.vue
@@ -1,15 +1,27 @@
 <script setup>
-import { onMounted, onUnmounted, ref } from 'vue';
+import { onMounted, onUnmounted, ref, onBeforeMount, onBeforeUnmount } from 'vue';
 
 const time = ref(0);
 let intervalId = null
+
+onBeforeMount(() => {
+  console.log('on before mount');
+})
 onMounted(() => {
+  console.log("on mounted");
+  
  intervalId = setInterval(() => {
   time.value++
  },1000)
 });
+
+onBeforeUnmount(() => {
+  console.log('on before unmount');
+})
 // Khi thoát khỏi trang này nó sẽ chạy hàm Unmounted
 onUnmounted(() => {
+  console.log("on Unmoutedf");
+  
   clearInterval(intervalId)
 })
 </script>
