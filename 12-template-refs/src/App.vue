b0343cf53b8687ef91049e75c72f9707e81fc7e5
hieudm05 <hieudm05@gmail.com>
2025-12-04 09:45:25 +0700

    refs

---
+++ 12-template-refs/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const inputRef = ref(null)
+onMounted(() => {
+  inputRef.value.focus()
+})
+</script>
+<template>
+  <h1>Template refs</h1>
+  <input type="text" ref="inputRef">
+<button @click="changeAge">Change</button>
+</template>
