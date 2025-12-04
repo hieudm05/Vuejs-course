21654a81c7b1e9dae17426d62914ae6581be2f61
hieudm05 <hieudm05@gmail.com>
2025-12-04 10:40:03 +0700

    ref với v-for

--- 12-template-refs/src/App.vue
+++ 12-template-refs/src/App.vue
@@ -1,13 +1,28 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { nextTick, onMounted, ref } from 'vue';
+// Khi nào cần sử dụng refs: 
+// Khi mà ta muốn thao tác với DOM trực tiếp
+// Muốn thao tác với các thư viện trên JS bên ngoài (jquery)
+// Cần quản lý trạng thái của các phần tử
+// Tối ưu hiệu suất (tìm kiếm DOM nhanh chóng)
 
-const inputRef = ref(null)
-onMounted(() => {
-  inputRef.value.focus()
-})
+  const list = ref(["Item1", "Item2", "Item3"]);
+  const itemsList = ref([])
+ onMounted(async () => {
+  await nextTick();
+  itemsList.value.forEach(item => {
+    item.style.transition = "opacity 10s";
+    // requestAnimationFrame: cho trình duyệt đợi lại 1 nhịp để chạy từng hiệu ứng
+    requestAnimationFrame(() => {
+      item.style.opacity = 1;
+      item.style.color = "red"
+    })
+  });
+});
 </script>
 <template>
-  <h1>Template refs</h1>
-  <input type="text" ref="inputRef">
-<button @click="changeAge">Change</button>
+  <h1>Refs với v-for</h1>
+  <ul>
+  <li v-for="(value, index) in list"  :key="index" ref="itemsList" style="opacity: 0"> {{ value }}</li>
+  </ul>
 </template>
